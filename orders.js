86859688cfd574f9b6d12fc1157ca45fab304e3d
Sharaf <114002993+MBSharaf@users.noreply.github.com>
2023-12-13 23:20:01 +0200

    Add files via upload

---
+++ orders.js
@@ -0,0 +1,44 @@
+const Orders = [
+  {
+    productName: 'Laptop',
+    productNumber: 'ABC123',
+    paymentStatus: 'Paid',
+    shipping: 'Shipped'
+  },
+  {
+    productName: 'Smartphone',
+    productNumber: 'XYZ456',
+    paymentStatus: 'Pending',
+    shipping: 'Processing'
+  },
+  {
+    productName: 'Headphones',
+    productNumber: 'DEF789',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+  {
+    productName: 'Camera',
+    productNumber: 'GHI321',
+    paymentStatus: 'Paid',
+    shipping: 'Shipped'
+  },
+  {
+    productName: 'Fitness Tracker', 
+    productNumber: 'JKL654',
+    paymentStatus: 'Pending',
+    shipping: 'Processing'
+  },
+  {
+    productName: 'Desk Chair',
+    productNumber: 'MNO987',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+  },
+{
+    productName: 'Desk Chair',
+    productNumber: 'MNO987',
+    paymentStatus: 'Paid',
+    shipping: 'Delivered'
+    },
+];
